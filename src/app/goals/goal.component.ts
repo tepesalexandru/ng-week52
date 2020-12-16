8b538b4a56f4fd5e6f2721a192c83101c92a91d7
Tepes Alexandru <godzplayer@hotmail.com>
2020-12-16 17:36:02 +0200

    added basic styles

--- src/app/goals/goal.component.ts
+++ src/app/goals/goal.component.ts
@@ -4,6 +4,7 @@ import {IGoal} from '../shared/interfaces/goal';
 @Component({
   selector: 'goal',
   templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
 })
 export class GoalComponent {
   @Input() goal: IGoal;
